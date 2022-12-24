36e44271cba7ec8159174ba18b4a7d733550921a
jgalesp <116549383+jgalesp@users.noreply.github.com>
2022-12-24 16:37:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ cabecera('Consultas Preparadas');
 echo "<h1>CONTROL DE ASISTENCIA EN D.A.W.</h1>";
 echo "<img src='tablas.jpg'>"; 
 		echo "</div>";
+
+echo "<a href='meter_faltas.php'>Seleccionar curso</a>";
 		pie();
 ?>
