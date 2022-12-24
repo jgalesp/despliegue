d053012739b8981da16d72ab10146d41ec877206
jgalesp <116549383+jgalesp@users.noreply.github.com>
2022-12-24 17:12:52 +0100

    Update selec_curso.php

--- selec_curso.php
+++ selec_curso.php
@@ -1,18 +1,3 @@
 <?php
-include('/app/funciones.php');
-include('/app/funcionesBD.php');
-cabecera('Seleccione curso');
-echo "<div id=\"contenido\">\n";
-
-$conexion=new mysqli("containers-us-west-189.railway.app","root","ufMOoOlCZaCJkTDWxWr5","railway");
-$conexion->set_charset("utf8");
-$arrayCursos=obtenerArrayOpciones("alumnos","curso","curso");
-
-echo "<form id=\"form_seleccion\" action=\"meter_faltas.php\" method=\"post\">";
-echo "Curso:";
-pintarCombo($arrayCursos,"curso");
-
-echo "<input type=submit value='Introducir faltas' name='enviar'></form>";
-echo "</div>";
-pie();
+echo "PEPE"
 ?>
