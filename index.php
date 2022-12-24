afe209d8adac70a2a35bffa4ef1969e49f6b2be1
jgalesp <116549383+jgalesp@users.noreply.github.com>
2022-12-24 17:23:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include('/app/funciones.php');
+include_once "/app/funciones.php";
 cabecera('Consultas Preparadas');
 echo "<h1>CONTROL DE ASISTENCIA EN D.A.W.</h1>";
 echo "<img src='tablas.jpg'>"; 
